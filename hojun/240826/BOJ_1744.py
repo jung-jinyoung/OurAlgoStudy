cbfb56c7e912c50284e38f8294f40279450b7acc
poppop1818 <poppop1818@naver.com>
2024-09-02 05:38:57 +0900

    refactor : 수 묶기 해결

--- hojun/240826/BOJ_1744.py
+++ hojun/240826/BOJ_1744.py
@@ -1,24 +1,41 @@
 # 수 묶기
 import sys
+
 input = sys.stdin.readline
-
 N = int(input())
-nums = sorted([int(input()) for _ in range(N)])  # 오름 차순 정렬
-a = -1001
-b = -1001
-result = 0
 
-while nums:
-  if a == -1001:
-    a = nums.pop()
-  elif b == -1001:
-    b = nums.pop()
-    if a*b >= a+b:
-      result += a*b
-      a, b = -1001, -1001
+plus = []   #1이상 양수 저장할 리스트
+minus = []  #마이너스와 0 저장할 리스트
+one = 0 #1은 그냥 무조건 혼자 더해지는게 최고임
+
+for _ in range(N):
+    num = int(input())
+    if num > 1:
+        plus.append(num)
+    elif num <= 0:
+        minus.append(num)
     else:
-      result += a + b
-      a, b = -1001, -1001
-if a != -1001:
-  result += a
-  print(result)
+        one += 1
+
+plus.sort()
+minus.sort(reverse=True)
+
+result = one
+
+while len(plus)//2 > 0:
+    a = plus.pop()
+    b = plus.pop()
+    result += a*b
+
+if plus:
+    result += sum(plus)
+
+while len(minus)//2 > 0:
+    a = minus.pop()
+    b = minus.pop()
+    result += a*b
+
+if minus:
+    result += sum(minus)
+
+print(result)
