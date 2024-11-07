a1c1c1c15ca63b26ee9c784214563736ad53cdac
yuan <whyuan@gmail.com>
2024-11-07 21:51:50 +0900

    feat:PSG_거리두기 확인하기 solved

--- yuan/241104/PSG_숫자 문자열과 영단어.py
+++ yuan/241104/PSG_숫자 문자열과 영단어.py
@@ -30,26 +30,3 @@ def solution(places):
         now = check(place)
         answer.append(now)
     return answer
-
-
-"""
-def solution(s):
-    dict = { 'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
-    answer = ''
-    # print(numbers)
-    now_s=''
-    for i in s:
-        if i.isdigit(): # 숫자일떄
-            if now_s !='':
-                answer+=dict[now_s]
-                now_s = ''
-            answer+=i
-        else: #문자일떄
-            if now_s in dict:
-                answer+=dict[now_s]
-                now_s = ''
-            now_s += i
-    if now_s !='':
-        answer+=dict[now_s]
-    return int(answer)
-"""
